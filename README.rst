3e5f8e9d6abe734a6a8886a6a02eb9b8178491dc
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-09-13 21:57:12 +0300

    first commit

---
+++ README.rst
@@ -0,0 +1,1 @@
+See `dy/fi/docs/README.rst <https://github.com/taito/dy.fi/blob/master/dy/fi/docs/README.rst>`_
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='dy.fi',
+    version='0.0',
+    description="Checks global IP address change and informs currenty IP address to dy.fi.",
+    long_description=open("README.rst").read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7"],
+    keywords='',
+    author='Taito Horiuchi',
+    author_email='taito.horiuchi@gmail.com',
+    url='',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['dy'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools'],
+    entry_points="""
+    """)
